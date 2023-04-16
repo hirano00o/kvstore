5ee473746a2674dddd838655fb361e3756cc4568
hirano00o <uday6827@gmail.com>
2023-04-16 20:30:39 +0900

    Add freelist feature

--- dal.go
+++ dal.go
@@ -15,6 +15,8 @@ type page struct {
 type dal struct {
 	file     *os.File
 	pageSize int
+
+	*freeList
 }
 
 func newDal(path string, pageSize int) (*dal, error) {
@@ -25,6 +27,7 @@ func newDal(path string, pageSize int) (*dal, error) {
 	dal := &dal{
 		file:     f,
 		pageSize: pageSize,
+		freeList: newFreeList(),
 	}
 
 	return dal, nil
