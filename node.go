86d1270bc762dc048b2019ed74bd5428fd3df67a
hirano00o <uday6827@gmail.com>
2023-04-20 10:49:21 +0900

    Add constructor for node

---
+++ node.go
@@ -0,0 +1,29 @@
+package kvstore
+
+type Item struct {
+	key   []byte
+	value []byte
+}
+
+type Node struct {
+	*dal
+
+	pageNum    pgnum
+	items      []*Item
+	childNodes []pgnum
+}
+
+func NewEmptyNode() *Node {
+	return &Node{}
+}
+
+func newItem(key, value []byte) *Item {
+	return &Item{
+		key:   key,
+		value: value,
+	}
+}
+
+func (n *Node) isLeaf() bool {
+	return len(n.childNodes) == 0
+}
